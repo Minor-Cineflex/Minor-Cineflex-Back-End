c79efa2db317969c03f258bc6e00d50c72c7936b
Boonwarat <Boonwarat074@gmail.com>
2025-03-10 21:15:47 +0700

    and die with a smileeeeee

--- Backend/main.py
+++ Backend/main.py
@@ -1480,7 +1480,32 @@ def email(email: str):
                             "point": p.account.point,
                             "registered_date": p.account.registered_date,
                             "expiration_date": p.account.expiration_date,
-                            "history": p.account.history,
+                            "history":[
+                                BookingResponse(
+                                    showtime=ShowtimeResponse(
+                                        showtime_id=b.showtime.showtime_id,
+                                        start_date=b.showtime.start_date,
+                                        cinema_id=b.showtime.cinema_id,
+                                        theater_id=b.showtime.theater_id,
+                                        movie_id=b.showtime.movie_id,
+                                        dub=b.showtime.dub,
+                                        sub=b.showtime.sub
+                                    ),
+                                    account_id=b.account_id,
+                                    seat_list=[
+                                    SeatForSeatResponse(
+                                            seat_id=seat.seat_id,
+                                            seat_type=seat.seat_type,
+                                            size=seat.size,
+                                            price=seat.price,
+                                            seat_pos=seat.seat_pos
+                                        ) for seat in b.seat_list
+                                    ],
+                                    booking_date=b.booking_date,
+                                    payment_method=PaymentResponse(payment_type=b.payment_method.payment_type),
+                                    total=b.total
+                                ) for b in p.account.history
+                            ],
                             "document_list": p.account.document_list,
                             "reserved_list": p.account.reserved_list
                         }
